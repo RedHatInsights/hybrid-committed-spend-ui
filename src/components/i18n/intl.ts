fc0168fd56958efbc93ed7fede939573889c111e
Dan Labrecque <dlabrecq@redhat.com>
2022-08-28 21:46:49 -0400

    Added custom webpack config, so we don't have to use relative import paths

--- src/components/i18n/intl.ts
+++ src/components/i18n/intl.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line no-restricted-imports
 import { createIntl, createIntlCache } from 'react-intl';
 
+// eslint-disable-next-line no-restricted-imports
 import messages from '../../../locales/data.json';
 
 const locale = navigator.language.split(/[-_]/)[0] || 'en';
