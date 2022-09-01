f236cc6afd8c965ef963749c0c456fbe1b192877
Dan Labrecque <dlabrecq@redhat.com>
2022-09-01 14:57:52 -0400

    Added Apis

---
+++ src/api/api.ts
@@ -0,0 +1,41 @@
+import { authInterceptor as insightsAuthInterceptor } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
+import axios, { AxiosRequestConfig } from 'axios';
+
+export interface PagedMetaData {
+  count: number;
+}
+
+export interface PagedLinks {
+  first: string;
+  previous: string;
+  next: string;
+  last: string;
+}
+
+export interface PagedResponse<D = any, M = any> {
+  meta: M;
+  links: PagedLinks;
+  data: D[];
+}
+
+export function initApi({ version }: { version: string }) {
+  axios.defaults.baseURL = `https://billing.dev.api.redhat.com/${version}/`;
+  axios.interceptors.request.use(authInterceptor);
+  axios.interceptors.request.use(insightsAuthInterceptor);
+}
+
+export function authInterceptor(reqConfig: AxiosRequestConfig): AxiosRequestConfig {
+  const insights = (window as any).insights;
+  return insights.chrome.auth.getToken().then(token => {
+    if (!token) {
+      return reqConfig;
+    }
+    return {
+      ...reqConfig,
+      headers: {
+        ...reqConfig.headers,
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  });
+}
