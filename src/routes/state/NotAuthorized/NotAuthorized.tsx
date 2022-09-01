a90a2ecce76301852aa7a33f3f8651dce4cf55db
Dan Labrecque <dlabrecq@redhat.com>
2022-09-01 14:59:08 -0400

    Added state components

---
+++ src/routes/state/NotAuthorized/NotAuthorized.tsx
@@ -0,0 +1,30 @@
+import Main from '@redhat-cloud-services/frontend-components/Main';
+import PageHeader, { PageHeaderTitle } from '@redhat-cloud-services/frontend-components/PageHeader';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+import { NotAuthorizedState } from './NotAuthorizedState';
+
+interface NotAuthorizedOwnProps {
+  pathname?: string;
+  title?: string;
+}
+
+type NotAuthorizedProps = NotAuthorizedOwnProps & RouteComponentProps<void>;
+
+const NotAuthorized = ({ pathname, title }: NotAuthorizedProps) => {
+  return (
+    <>
+      {title && (
+        <PageHeader>
+          <PageHeaderTitle title={title} />
+        </PageHeader>
+      )}
+      <Main>
+        <NotAuthorizedState pathname={pathname} />
+      </Main>
+    </>
+  );
+};
+
+export default withRouter(NotAuthorized);
---
+++ src/routes/state/index.ts
@@ -0,0 +1,4 @@
+export * from './Loading';
+export * from './NotAuthorized';
+export * from './NotAvailable';
+export * from './NotFound';
