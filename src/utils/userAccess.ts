f1ccbb355e9c0431fdd3d602f85973c69311a084
Dan Labrecque <dlabrecq@redhat.com>
2022-09-02 19:35:44 -0400

    Added component to request user access to page and features

---
+++ src/utils/userAccess.ts
@@ -0,0 +1,25 @@
+import { UserAccess, UserAccessType } from 'api/userAccess';
+
+const hasAccess = (userAccess: UserAccess, userAccessType: UserAccessType) => {
+  let result = false;
+
+  if (userAccess && Array.isArray(userAccess.data)) {
+    // Used with multiple types (e.g., type=)
+    const data = (userAccess.data as any).find(d => d.type === userAccessType);
+    result = data && data.access;
+  } else {
+    // result = userAccess && userAccess.data === true;
+    result = true; // Todo: Remove when API is available
+  }
+  return result;
+};
+
+// Returns true if user has access to all billing
+export const hasAllAccess = (userAccess: UserAccess) => {
+  return hasAccess(userAccess, UserAccessType.all);
+};
+
+// Returns true if user has access to AWS and at least one source provider
+export const isAwsAvailable = (userAccess: UserAccess) => {
+  return hasAllAccess(userAccess);
+};
