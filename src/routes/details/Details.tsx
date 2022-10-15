2552cd059748ae4e1db650eb94f0a1f1f3455a20
Dan Labrecque <dlabrecq@redhat.com>
2022-10-15 00:03:44 -0400

    Updated details page

--- src/routes/details/Details.tsx
+++ src/routes/details/Details.tsx
@@ -1,24 +1,53 @@
+import { Bullseye, Pagination, PaginationVariant, Spinner } from '@patternfly/react-core';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
-import React, { lazy, Suspense } from 'react';
+import { getQuery, Query } from 'api/queries';
+import { Report, ReportPathsType, ReportType } from 'api/reports';
+import { AxiosError } from 'axios';
+import messages from 'locales/messages';
+import React, { lazy, Suspense, useMemo, useState } from 'react';
 import { injectIntl, WrappedComponentProps } from 'react-intl';
+import { useSelector } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { ExportModal } from 'routes/components/export';
+import {
+  handleOnFilterAdded,
+  handleOnFilterRemoved,
+  handleOnPerPageSelect,
+  handleOnSetPage,
+} from 'routes/utils/history';
+import { RootState } from 'store';
+import { FetchStatus } from 'store/common';
+import { reportActions, reportSelectors } from 'store/reports';
+import { getIdKeyForGroupBy } from 'utils/computedReport/getComputedDetailsReportItems';
+import { getUnsortedComputedReportItems } from 'utils/computedReport/getComputedReportItems';
 
+import { styles } from './Details.styles';
+import { DetailsHeader } from './DetailsHeader';
+import { DetailsHeaderToolbar } from './DetailsHeaderToolbar';
+import { TableToolbar } from './TableToolbar';
+import {
+  DateRangeType,
+  getDateRangeType,
+  getGroupByType,
+  getSourcesOfSpendType,
+  GroupByType,
+  SourcesOfSpendType,
+} from './utils';
+
+const Loading = lazy(() => import('routes/state/loading/Loading'));
 const NotAvailable = lazy(() => import('routes/state/not-available/NotAvailable'));
 
-import { Bullseye, Spinner } from '@patternfly/react-core';
-import { connect } from 'react-redux';
-import { createMapStateToProps } from 'store/common';
-import { reportSelectors } from 'store/reports';
-
-import DetailsToolbar from './DetailsToolbar';
-import { DetailsHeader } from './index';
-
 interface DetailsOwnProps {
-  // TBD...
+  groupBy?: string;
 }
 
 interface DetailsStateProps {
   hasReportErrors: boolean;
+  query: Query;
+  queryString: string;
+  report: Report;
+  reportError: AxiosError;
+  reportFetchStatus: FetchStatus;
 }
 
 interface DetailsDispatchProps {
@@ -31,67 +60,206 @@ type DetailsProps = DetailsOwnProps &
   RouteComponentProps<void> &
   WrappedComponentProps;
 
-const DetailsBase: React.FC<DetailsProps> = ({ hasReportErrors }) => {
+export const baseQuery: Query = {
+  filter: {
+    limit: 10,
+    offset: 0,
+  },
+  filter_by: {},
+  order_by: {
+    cost: 'desc',
+  },
+};
+
+const Details: React.FC<DetailsProps> = ({ history, intl }) => {
+  const [dateRange, setDateRange] = useState(getDateRangeType(DateRangeType.contractedYtd));
+  const [groupBy, setGroupBy] = useState(getGroupByType(GroupByType.product));
+  const [isExportModalOpen, setIsExportModalOpen] = useState(false);
+  const [secondaryGroupBy, setSecondaryGroupBy] = useState(GroupByType.none);
+  const [sourcesOfSpend, setSourcesOfSpend] = useState(getSourcesOfSpendType(SourcesOfSpendType.all));
+
+  const { hasReportErrors, query, report, reportFetchStatus } = mapToProps({
+    groupBy,
+  });
+
+  const getComputedItems = () => {
+    const computedItems = getUnsortedComputedReportItems({
+      report,
+      idKey: getIdKeyForGroupBy(query.group_by),
+      isDateMap: false, // Don't use isDateMap here, so we can use a flattened data structure with row selection
+    });
+    return computedItems;
+  };
+
+  const getDataToolbar = () => {
+    const computedItems = getComputedItems();
+
+    return (
+      <TableToolbar
+        groupBy={groupBy}
+        isExportDisabled={computedItems.length !== 0}
+        onExportClicked={handleOnExportModalOpen}
+        onFilterAdded={filter => handleOnFilterAdded(history, query, filter)}
+        onFilterRemoved={filter => handleOnFilterRemoved(history, query, filter)}
+        pagination={getPagination()}
+      />
+    );
+  };
+
+  const getExportModal = () => {
+    const groupById = getGroupByType(query.group_by);
+
+    return (
+      <ExportModal
+        groupBy={groupById}
+        isOpen={isExportModalOpen}
+        onClose={handleOnExportModalClose}
+        query={query}
+        reportPathsType={ReportPathsType.billing}
+      />
+    );
+  };
+
+  const getPagination = (isBottom: boolean = false) => {
+    const count = report && report.meta && report.meta.count !== undefined ? report.meta.count : 0;
+    const limit =
+      report && report.meta && report.meta.filter && report.meta.filter.limit ? report.meta.filter.limit : 0;
+    const offset =
+      report && report.meta && report.meta.filter && report.meta.filter.offset ? report.meta.filter.offset : 0;
+    const page = offset > 0 ? offset / limit + 1 : 0;
+
+    return (
+      <Pagination
+        isCompact={!isBottom}
+        itemCount={count}
+        onPerPageSelect={(event, perPage) => handleOnPerPageSelect(history, query, perPage)}
+        onSetPage={(event, pageNumber) => handleOnSetPage(history, query, report, pageNumber)}
+        page={page}
+        perPage={limit}
+        titles={{
+          paginationTitle: intl.formatMessage(messages.paginationTitle, {
+            title: intl.formatMessage(messages.detailsTitle),
+            placement: isBottom ? 'bottom' : 'top',
+          }),
+        }}
+        variant={isBottom ? PaginationVariant.bottom : PaginationVariant.top}
+        widgetId={`pagination${isBottom ? '-bottom' : ''}`}
+      />
+    );
+  };
+
   const handleOnDateRangeSelected = value => {
-    // eslint-disable-next-line no-console
-    console.log('handleOnDateRangeSelected', handleOnDateRangeSelected);
+    setDateRange(value);
+  };
+
+  const handleOnExportModalClose = (isOpen: boolean) => {
+    setIsExportModalOpen(isOpen);
+  };
+
+  const handleOnExportModalOpen = () => {
+    setIsExportModalOpen(true);
   };
 
   const handleOnGroupBySelected = value => {
-    // eslint-disable-next-line no-console
-    console.log('handleOnGroupBySelected', handleOnGroupBySelected);
+    setGroupBy(value);
   };
 
   const handleOnSecondaryGroupBySelected = value => {
-    // eslint-disable-next-line no-console
-    console.log('handleOnSecondaryGroupBySelected', handleOnSecondaryGroupBySelected);
+    setSecondaryGroupBy(value);
   };
 
   const handleOnSourcesOfSpendSelected = value => {
-    // eslint-disable-next-line no-console
-    console.log('handleOnSourcesOfSpendSelected', handleOnSourcesOfSpendSelected);
+    setSourcesOfSpend(value);
   };
 
   // Todo: Remove when APIs are available
   const isTest = true;
 
+  if (hasReportErrors && !isTest) {
+    const title = intl.formatMessage(messages.detailsTitle);
+    return <NotAvailable title={title} />;
+  }
+
   return (
     <React.Fragment>
-      {!isTest && hasReportErrors ? (
-        <NotAvailable />
-      ) : (
-        <>
-          <DetailsHeader>
-            <DetailsToolbar
-              onDateRangeSelected={handleOnDateRangeSelected}
-              onGroupBySelected={handleOnGroupBySelected}
-              onSecondaryGroupBySelected={handleOnSecondaryGroupBySelected}
-              onSourcesOfSpendSelected={handleOnSourcesOfSpendSelected}
-            />
-          </DetailsHeader>
-          <Main>
-            <Suspense
-              fallback={
-                <Bullseye>
-                  <Spinner size="lg" />
-                </Bullseye>
-              }
-            >
-              <span>Hello</span>
-            </Suspense>
-          </Main>
-        </>
-      )}
+      <DetailsHeader>
+        <DetailsHeaderToolbar
+          dateRange={dateRange}
+          groupBy={groupBy}
+          onDateRangeSelected={handleOnDateRangeSelected}
+          onGroupBySelected={handleOnGroupBySelected}
+          onSecondaryGroupBySelected={handleOnSecondaryGroupBySelected}
+          onSourcesOfSpendSelected={handleOnSourcesOfSpendSelected}
+          secondaryGroupBy={secondaryGroupBy}
+          sourcesOfSpend={sourcesOfSpend}
+        />
+      </DetailsHeader>
+      <Main>
+        <Suspense
+          fallback={
+            <Bullseye>
+              <Spinner size="lg" />
+            </Bullseye>
+          }
+        >
+          {getDataToolbar()}
+          {getExportModal()}
+          {reportFetchStatus === FetchStatus.inProgress ? (
+            <Loading />
+          ) : (
+            <React.Fragment>
+              <div>Table placeholder</div>
+              <div style={styles.pagination}>{getPagination(true)}</div>
+            </React.Fragment>
+          )}
+        </Suspense>
+      </Main>
     </React.Fragment>
   );
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const mapStateToProps = createMapStateToProps<DetailsOwnProps, DetailsStateProps>((state, props) => {
-  return {
-    hasReportErrors: reportSelectors.selectHasErrors(state),
-  };
-});
+const mapToProps = ({ groupBy }: DetailsOwnProps): DetailsStateProps => {
+  const reportPathsType = ReportPathsType.billing;
+  const reportType = ReportType.cost;
 
-const Details = withRouter(DetailsBase);
-export default injectIntl(connect(mapStateToProps, {})(Details));
+  const query = {
+    group_by: groupBy,
+  };
+  const queryString = getQuery(query);
+
+  const hasReportErrors = useSelector((state: RootState) => reportSelectors.selectHasErrors(state));
+
+  const report = useSelector((/* state: RootState */) => {
+    // reportSelectors.selectReport(state, widget.reportPathsType, widget.reportType, queryString)
+    return {
+      meta: {
+        total: {
+          value: 0,
+          units: 'USD',
+        },
+      },
+    } as any;
+  });
+  const reportFetchStatus = useSelector((state: RootState) =>
+    reportSelectors.selectReportFetchStatus(state, reportPathsType, reportType, queryString)
+  );
+  const reportError = useSelector((state: RootState) =>
+    reportSelectors.selectReportError(state, reportPathsType, reportType, queryString)
+  );
+
+  useMemo(() => {
+    // Todo: Enable via dispatch
+    reportActions.fetchReport(reportPathsType, reportType, queryString);
+  }, [queryString]);
+
+  return {
+    hasReportErrors,
+    query,
+    queryString,
+    report,
+    reportFetchStatus,
+    reportError,
+  };
+};
+
+export default injectIntl(withRouter(Details));
---
+++ src/routes/details/utils.ts
@@ -0,0 +1,89 @@
+// eslint-disable-next-line no-shadow
+export enum DateRangeType {
+  contractedYtd = 'contracted_ytd',
+  contractedLastYear = 'contracted_last_year',
+  date = 'date',
+  lastNineMonths = 'last_nine_months',
+  lastSixMonths = 'last_six_months',
+  lastThreeMonths = 'last_three_months',
+}
+
+// eslint-disable-next-line no-shadow
+export enum GroupByType {
+  account = 'account',
+  affiliate = 'affiliate',
+  none = 'none',
+  product = 'product',
+  sourceOfSpend = 'source_of_spend',
+}
+
+// eslint-disable-next-line no-shadow
+export enum SourcesOfSpendType {
+  all = 'all',
+  aws = 'aws',
+  azure = 'azure',
+  consulting = 'consulting',
+  gcp = 'gcp',
+  marketplace = 'marketplace',
+  reseller = 'reseller',
+  subs_on_demand = 'subs_on_demand',
+  subs_yearly = 'subs_yearly',
+}
+
+export function getDateRangeType(dateRange: DateRangeType): string {
+  switch (dateRange) {
+    case DateRangeType.contractedYtd:
+      return 'contracted_ytd';
+    case DateRangeType.contractedLastYear:
+      return 'contracted_last_year';
+    case DateRangeType.lastNineMonths:
+      return 'last_nine_months';
+    case DateRangeType.lastSixMonths:
+      return 'last_six_months';
+    case DateRangeType.lastThreeMonths:
+      return 'last_three_months';
+    case DateRangeType.date:
+    default:
+      return 'date';
+  }
+}
+
+export function getGroupByType(groupBy: GroupByType): string {
+  switch (groupBy) {
+    case GroupByType.account:
+      return 'account';
+    case GroupByType.affiliate:
+      return 'affiliate';
+    case GroupByType.product:
+      return 'product';
+    case GroupByType.sourceOfSpend:
+      return 'source_of_spend';
+    case GroupByType.none:
+    default:
+      return 'none';
+  }
+}
+
+export function getSourcesOfSpendType(sourcesOfSpend: SourcesOfSpendType): string {
+  switch (sourcesOfSpend) {
+    case SourcesOfSpendType.aws:
+      return 'aws';
+    case SourcesOfSpendType.azure:
+      return 'azure';
+    case SourcesOfSpendType.consulting:
+      return 'consulting';
+    case SourcesOfSpendType.gcp:
+      return 'gcp';
+    case SourcesOfSpendType.marketplace:
+      return 'marketplace';
+    case SourcesOfSpendType.reseller:
+      return 'reseller';
+    case SourcesOfSpendType.subs_on_demand:
+      return 'subs_on_demand';
+    case SourcesOfSpendType.subs_yearly:
+      return 'subs_yearly';
+    case SourcesOfSpendType.all:
+    default:
+      return 'all';
+  }
+}
