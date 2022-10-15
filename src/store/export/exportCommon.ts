aa38bc50d904eb78ee3b405051d829ddf20dc990
Dan Labrecque <dlabrecq@redhat.com>
2022-10-14 23:54:55 -0400

    Added initial export feature

---
+++ src/store/export/exportCommon.ts
@@ -0,0 +1,7 @@
+import { ReportPathsType, ReportType } from 'api/reports/report';
+
+export const exportStateKey = 'export';
+
+export function getExportId(reportPathsType: ReportPathsType, reportType: ReportType, query: string) {
+  return `${reportPathsType}-${reportType}--${query}`;
+}
---
+++ src/store/export/index.ts
@@ -0,0 +1,6 @@
+import * as exportActions from './exportActions';
+import { exportStateKey } from './exportCommon';
+import { ExportAction, exportReducer, ExportState } from './exportReducer';
+import * as exportSelectors from './exportSelectors';
+
+export { ExportAction, exportActions, exportReducer, exportSelectors, ExportState, exportStateKey };
