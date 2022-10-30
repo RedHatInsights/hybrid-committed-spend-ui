25419099828c225a807f68f26703157676ca0611
Dan Labrecque <dlabrecq@redhat.com>
2022-10-30 11:11:20 -0400

    Cleaned up report properties

--- src/api/reports/detailsFilterReports.ts
+++ src/api/reports/detailsFilterReports.ts
@@ -1,18 +1,10 @@
 import axios from 'axios';
 
-import type { Report, ReportData, ReportMeta, ReportValue } from './report';
+import type { Report, ReportData, ReportMeta } from './report';
 import { ReportType } from './report';
 
 export interface DetailsFilterReportData extends ReportData {
-  account_name?: string;
-  account_number?: string;
-  contract_start_date?: string;
-  contract_end_date?: string;
-  consumption_date?: string;
-  committed_spend?: ReportValue;
-  delta: { value: string; percent: string };
-  excess_committed_spend?: ReportValue;
-  remaining_committed_spend?: ReportValue;
+  value?: string;
 }
 
 export interface DetailsFilterReportMeta extends ReportMeta {
