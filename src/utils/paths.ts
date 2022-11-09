a880b122e9e46362291bfe99ba93537786c5c547
Dan Labrecque <dlabrecq@redhat.com>
2022-11-09 14:42:42 -0500

    Added getPath comment

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,5 +1,6 @@
 import { routes } from 'Routes';
 
+// Returns Routes path associated with given window location
 export const getPath = location => {
   const currRoute = location ? routes.find(({ path }) => path === location.pathname) : undefined;
   return currRoute ? currRoute.path : undefined;
