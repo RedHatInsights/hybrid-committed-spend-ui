800faaed74a8eff13c07a3fba408e25a0577eb55
Dan Labrecque <dlabrecq@redhat.com>
2023-04-04 13:00:57 -0400

    Revert previous API change to call insights.chrome.auth.getUser

--- src/api/options/detailsOption.ts
+++ src/api/options/detailsOption.ts
@@ -21,12 +21,5 @@ export const OptionTypePaths: Partial<Record<OptionType, string>> = {
 export function runOption(reportType: OptionType, query: string) {
   const path = OptionTypePaths[reportType];
   const queryString = query ? `?${query}` : '';
-  const fetch = () => axios.get<DetailsOption>(`${path}${queryString}`);
-
-  const insights = (window as any).insights;
-  if (insights && insights.chrome && insights.chrome.auth && insights.chrome.auth.getUser) {
-    return insights.chrome.auth.getUser().then(() => fetch());
-  } else {
-    return fetch();
-  }
+  return axios.get<DetailsOption>(`${path}${queryString}`);
 }
--- src/api/reports/accountSummaryReport.ts
+++ src/api/reports/accountSummaryReport.ts
@@ -37,12 +37,5 @@ export const ReportTypePaths: Partial<Record<ReportType, string>> = {
 export function runReport(reportType: ReportType, query: string) {
   const path = ReportTypePaths[reportType];
   const queryString = query ? `?${query}` : '';
-  const fetch = () => axios.get<AccountSummaryReport>(`${path}${queryString}`);
-
-  const insights = (window as any).insights;
-  if (insights && insights.chrome && insights.chrome.auth && insights.chrome.auth.getUser) {
-    return insights.chrome.auth.getUser().then(() => fetch());
-  } else {
-    return fetch();
-  }
+  return axios.get<AccountSummaryReport>(`${path}${queryString}`);
 }
