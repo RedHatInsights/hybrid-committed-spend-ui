552b504fc8157850c65ef5fa671f577f95411b0f
Dan Labrecque <dlabrecq@redhat.com>
2023-07-27 13:52:11 -0400

    Update basename for Subscriptions bundle
    https://issues.redhat.com/browse/HCS-221

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom';
 import { routes } from 'Routes';
 
-// Prefixes the given path with a basename
-//
+// eslint-disable-next-line no-restricted-imports
+import pkg from '../../package.json';
+
 // Note the basename does not include a release prefix (/beta, /preview, etc.), unlike the getBaseName function from
 // @redhat-cloud-services/frontend-components-utilities/helpers
-export const formatPath = path => {
-  const basename = '/business-services/hybrid-committed-spend';
-  return path === routes.overview.path ? basename : `${basename}${path}`;
+export const getBasename = pathname => {
+  const index = pathname ? pathname.indexOf(pkg.insights.appname) + pkg.insights.appname.length : 0;
+  return index <= pathname.length ? pathname.substring(0, index) : ''; // '/business-services/hybrid-committed-spend'
 };
 
 export const getReleasePath = () => {
@@ -22,6 +23,13 @@ export const getReleasePath = () => {
     release = `/preview`;
   }
   return release;
+};
+
+// Prefixes the given path with a basename
+export const useFormatPath = path => {
+  const pathname = usePathname();
+  const basename = getBasename(pathname);
+  return path === routes.overview.path ? basename : `${basename}${path}`;
 };
 
 export const usePathname = () => {
