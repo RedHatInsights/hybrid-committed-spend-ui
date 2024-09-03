f31dee56348eb9d7ed5a0be1c24b79d420cc4bba
Dan Labrecque <dlabrecq@redhat.com>
2024-09-03 12:14:16 -0400

    PatternFly v6 milestone

--- src/store/feature-toggle/featureToggleActions.ts
+++ src/store/feature-toggle/featureToggleActions.ts
@@ -3,6 +3,7 @@ import { createAction } from 'typesafe-actions';
 export interface FeatureToggleActionMeta {
   isBillingStageToggleEnabled?: boolean;
   isDebugToggleEnabled?: boolean;
+  isOverridePermissionsToggleEnabled?: boolean;
 }
 
 export const setFeatureToggle = createAction('feature/init_feature_toggle')<FeatureToggleActionMeta>();
